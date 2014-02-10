8e991eb38c96fc466b71cd3249c81d1f8a7175b7
Suneel Kumar <suneel.kumar@phaedrasolutions.com>
2014-02-10 13:44:11 +0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ordering::Application.config.secret_token = '52cdae91f2be6eb8e57f4e87f4945655a6b3566439cc787590bbd9c9eed2ec206f5c8408e807beedcf703783ffc0f656b3035e8ef60d325f0c4630fa6d14f70c'
