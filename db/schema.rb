bac117ca95e1b257e06e41f43c9a532086d5a679
phaedra <phaedra@phaedra.(none)>
2014-05-23 18:11:27 +0500

    Alert working start

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140418074905) do
+ActiveRecord::Schema.define(:version => 20140523123425) do
 
   create_table "addon_subcategories", :force => true do |t|
     t.string   "name"
@@ -344,7 +344,10 @@ ActiveRecord::Schema.define(:version => 20140418074905) do
     t.integer  "avg_rating", :default => 0
     t.integer  "user_id"
     t.string   "status",     :default => "completed"
+    t.boolean  "delta",      :default => true,        :null => false
   end
+
+  add_index "restaurants", ["delta"], :name => "index_restaurants_on_delta"
 
   create_table "reviews", :force => true do |t|
     t.integer  "rating"
@@ -391,21 +394,22 @@ ActiveRecord::Schema.define(:version => 20140418074905) do
   create_table "users", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.integer  "sign_in_count",          :default => 0,     :null => false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "school_name"
     t.string   "customer_id"
     t.integer  "points",                 :default => 0
+    t.boolean  "mute",                   :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
