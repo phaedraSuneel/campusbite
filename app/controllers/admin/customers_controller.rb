e2146d415c817e5e06071bd7110c220deee52ebf
Suneel Kumar <suneel.kumar@phaedrasolutions.com>
2014-06-09 16:19:07 +0500

    show transcition of customer

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -22,6 +22,10 @@ class Admin::CustomersController < ApplicationController
       end
       format.html
     end
+
+    def show
+    	@user = User.where(id: params[:id]).first
+    end
 	end
  	
  	private
