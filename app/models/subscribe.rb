cc4c7c0d731f49861e42a74908fcac9348c05321
Suneel Kumar <suneel.kumar@phaedrasolutions.com>
2014-07-09 15:19:27 +0500

    Subscribe Form Issue fixed

--- app/models/subscribe.rb
+++ app/models/subscribe.rb
@@ -1,7 +1,7 @@
 class Subscribe < ActiveRecord::Base
   attr_accessible :email, :name, :school_name
   validates_uniqueness_of :email
-
+  validates_presence_of :email
   def self.apply_search_filter(data,key_word)
     search_keyword = ["%",key_word,"%"].join('')
     return data.where('name like ? OR school_name like ? OR email like ?', search_keyword, search_keyword, search_keyword)
